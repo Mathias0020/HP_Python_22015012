e54ff708b6e8afcb2803c7b7dffe3fb1ac84dcf5
Mathias0020 <135118291+Mathias0020@users.noreply.github.com>
2023-06-16 11:40:07 +0100

    Create 3-incremental_shape.py
    Assignment

---
+++ 3-incremental_shape.py
@@ -0,0 +1,9 @@
+#number of rows
+rows = 4
+for i in range (0,rows):
+    # nested loop for each column
+    for j in range (0,i+1):
+        #print harsh
+        print ("#", end="")
+    #new line after each row
+    print ("\r")
